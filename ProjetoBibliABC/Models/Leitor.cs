21a08d231d938a906e0dda920ca64d1accb70496
João Alexandre <102159971+JoaoAlexandre01@users.noreply.github.com>
2022-10-03 17:22:02 -0400

    Tentando Conectar o banco de dados

---
+++ ProjetoBibliABC/Models/Leitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBibliABC.Models
+{
+    internal class Leitor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string RG { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string Sexo { get; set; }
+    }
+}
+
+/*
+    CREATE TABLE Leitor(
+    id_lei int primary key auto_increment,
+    nome_lei varchar(100),
+    cpf_lei varchar(100),
+    email_lei varchar(100),
+    rg_lei varchar(100),
+    endereco_lei varchar(100),
+    telefone_lei varchar(100),
+    dataNascimento_lei date,
+     sexo_lei varchar(100)
+);
+ */
--- ProjetoBibliABC/Views/Cadastro_Leitor.xaml.cs
+++ ProjetoBibliABC/Views/Cadastro_Leitor.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using ProjetoBibliABC.Models;
+using ProjetoBibliABC.Views;
 
 namespace ProjetoBibliABC.Views
 {
@@ -19,12 +22,76 @@ namespace ProjetoBibliABC.Views
     /// </summary>
     public partial class Cadastro_Leitor : Window
     {
+        private Leitor _leitor = new Leitor();
         public Cadastro_Leitor()
         {
             InitializeComponent();
+            Loaded += Cadastro_Leitor_Loaded;
+        }
+        public Cadastro_Leitor(Leitor leitor)
+        {
+            InitializeComponent();
+            Loaded += Cadastro_Leitor_Loaded;
+            _leitor = leitor;
         }
 
+        private void Cadastro_Leitor_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtCodAce.Text = Convert.ToString(_leitor.Id);
+            txtNomeCompleto.Text = _leitor.Nome;
+            txtCPF.Text = _leitor.CPF;
+            txtRG.Text = _leitor.RG;
+            txtEmail.Text = _leitor.Email;
+            txtTelefone.Text = _leitor.Telefone;
+            dtDataNasc.SelectedDate = _leitor.DataNascimento;
+            txtRua.Text = _leitor.Endereco;
+        }
+
+        /*public Cadastro_Leitor()
+        {
+            InitializeComponent();
+        }*/
+
         private void btCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            _leitor.Id = Convert.ToInt32(txtCodAce.Text);
+            _leitor.Nome = txtNomeCompleto.Text;
+            _leitor.CPF = txtCPF.Text;
+            _leitor.RG = txtRG.Text;
+            _leitor.Email = txtEmail.Text;
+            _leitor.Telefone = txtTelefone.Text;
+            _leitor.DataNascimento = dtDataNasc.SelectedDate;
+            _leitor.Endereco = txtRua.Text;
+
+            try
+            {
+                var dao = new LeitorDAO();
+
+                if (_leitor.Id > 0)
+                {
+                    dao.Update(_leitor);
+                    MessageBox.Show("Registro de leitor atualizado com sucesso");
+                }
+                else
+                {
+                    dao.Insert(_leitor);
+
+                    MessageBox.Show("Registro de leitor cadastrado com sucesso.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btLimpar_Click(object sender, RoutedEventArgs e)
         {
 
         }
