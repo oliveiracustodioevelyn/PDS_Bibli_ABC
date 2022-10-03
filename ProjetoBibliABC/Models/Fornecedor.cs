3a6d8787c4630353deddda3ab8a89c222797da5a
duda340 <eduardarufino340@gmail.com>
2022-10-03 17:14:51 -0400

    up

---
+++ ProjetoBibliABC/Models/Fornecedor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBibliABC.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string NomeFornecedor { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Telefone { get; set; }
+    }
+}
+/*CREATE TABLE Fornecedor(
+id_for int primary key auto_increment,
+nomeEmpresa_for varchar(100),
+cnpj_for varchar(100),
+email_for varchar (100),
+razaoSocial_for varchar(100),
+endereco_for varchar(100),
+telefone_for varchar(100),
+descricao_for varchar(100) 
+); */
--- ProjetoBibliABC/Views/Cadastro_Livro.xaml.cs
+++ ProjetoBibliABC/Views/Cadastro_Livro.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ProjetoBibliABC.Models;
 
 namespace ProjetoBibliABC.Views
 {
@@ -19,11 +20,22 @@ namespace ProjetoBibliABC.Views
     /// </summary>
     public partial class Cadastro_Livro : Window
     {
+        private Livro _livro = new Livro();
+
         public Cadastro_Livro()
         {
             InitializeComponent();
             CbEditora.Items.Add("1");
             CbEditora.Items.Add("2");
+        }
+
+        private void Cadastro_Livro_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtAutor.Text = _livro.AutorObra;
+            txtEdicao.Text = _livro.EdicaoObra;
+            txtSinopse.Text = _livro.SinopseObra;
+            txtTitulo.Text = _livro.TituloObra;
+           
         }
 
         private void btHome_Click(object sender, RoutedEventArgs e)
@@ -55,5 +67,38 @@ namespace ProjetoBibliABC.Views
         {
 
         }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            _livro.SinopseObra = txtSinopse.Text;
+            _livro.AutorObra = txtAutor.Text;
+            _livro.EdicaoObra = txtEdicao.Text;
+            _livro.TituloObra = txtTitulo.Text;
+
+
+            try
+            {
+                var dao = new LivroDAO();
+
+                if (_livro.Id > 0)
+                {
+                    dao.Update(_livro);
+                    MessageBox.Show("Registro de curso atualizado com sucesso");
+                }
+                else
+                {
+                    dao.Insert(_livro);
+
+                    MessageBox.Show("Registro de escola cadastrado com sucesso.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
     }
 }
